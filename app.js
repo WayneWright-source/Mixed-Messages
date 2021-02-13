663fe2972850d184e5b6f54a39738ff70bd03ab6
Alex Wright <alexwright676@yahoo.com>
2021-02-12 23:23:01 -0500

    Message and function combined

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-Messages = ["Your limitation—it's only your imagination.", 
-"Push yourself, because no one else is going to do it for you.",
-"Sometimes later becomes never. ...",
-"Great things never come from comfort zones.",
-"Dream it. ...",
-"Success doesn't just find you. ...",
-"The harder you work for something, the greater you'll feel when you achieve it.",
-"Dream bigger."
-];
 
-console.log(Messages[3]);
+let randomMessage = (num) => {
+    Messages = ["Your limitation—it's only your imagination.", 
+    "Push yourself, because no one else is going to do it for you.",
+    "Sometimes later becomes never. ...",
+    "Great things never come from comfort zones.",
+    "Dream it. ...",
+    "Success doesn't just find you. ...",
+    "The harder you work for something, the greater you'll feel when you achieve it.",
+    "Dream bigger."
+    ];
+    console.log(Messages[num]);
+}
+
+
+randomMessage(digit);
